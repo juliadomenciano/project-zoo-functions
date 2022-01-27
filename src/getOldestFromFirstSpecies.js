b5351cdb3adc2191c4e008e6b05685e2c5d78553
Maju Domenciano <juliadomenciano@hotmail.com>
2022-01-27 19:13:06 -0300

    requisito 4

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((item) => item.id === id).responsibleFor[0];
+
+  const getAnimals = species.filter((animals) => animals.id === employee);
+  const sortAnimals = getAnimals[0].residents.sort((a, b) => b.age - a.age)[0];
+  return [sortAnimals.name, sortAnimals.sex, sortAnimals.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const managers = employees.some((item) => item.managers.some((elements) => elements === id));
+
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const arr = [];
+    employees.forEach((item) => {
+      item.managers.forEach((items) => {
+        if (items === managerId) {
+          arr.push(`${item.firstName} ${item.lastName}`);
+        }
+      });
+    });
+    return arr;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
